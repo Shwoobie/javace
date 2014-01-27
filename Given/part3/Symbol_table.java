820d76cc3029f01634345278188837250a7f4b2e
Shwoobie <atdwashington@gmail.com>
2014-01-27 15:25:31 -0800

    added correct {}

--- Given/part3/Symbol_table.java
+++ Given/part3/Symbol_table.java
@@ -56,12 +56,17 @@ public class Symbol_table {
          do{
             for(int i = 0; i < (tempSt.peek()).size(); i++){
             //System.err.println( "current name " + newSym.name + " itt name " + (sym_top().get(i)).name);
-            if((tempSym.name).equals((tempSt.peek()).get(i)).name))
+            if((tempSym.name).equals((tempSt.peek()).get(i)).name)){
                return true;
+            }
             }
             tempSt.pop();
             tempSym.depth--;
          }while(tempSym.depth > 0)
+
+      }
       return false;
+
    }
 }
+   
