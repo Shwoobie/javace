6c863d9b3d605623f2924002efc0533e77e9fcbe
Shwoobie <atdwashington@gmail.com>
2014-01-27 17:09:45 -0800

    moved it back

--- Given/part3/Symbol_table.java
+++ Given/part3/Symbol_table.java
@@ -52,6 +52,14 @@ public class Symbol_table {
 
    public boolean compare(Symbol newSym) {//compares names in the vector 
 
+      for(int i = 0; (i < sym_top().size()); i++){
+         System.err.println( "current name " + newSym.name + " itt name " + (sym_top().get(i)).name);
+         if((newSym.name).equals((sym_top().get(i)).name)){
+            System.err.println( "RETURN TRUE");
+            return true;
+         }
+         System.err.println( "CURRFALSE NAME " + newSym.name + " ITTFALSE NAME " + (sym_top().get(i)).name);
+      }
 
       if (newSym.nesting_depth > 0){
          Symbol tempSym = new Symbol(newSym.dec_line, newSym.nesting_depth, newSym.name);
@@ -70,15 +78,6 @@ public class Symbol_table {
                tempSt.pop();
                tempSym.nesting_depth--;
             }while(tempSym.nesting_depth >= 0);
-      }
-
-      for(int i = 0; (i < sym_top().size()); i++){
-         System.err.println( "current name " + newSym.name + " itt name " + (sym_top().get(i)).name);
-         if((newSym.name).equals((sym_top().get(i)).name)){
-            System.err.println( "RETURN TRUE");
-            return true;
-         }
-         System.err.println( "CURRFALSE NAME " + newSym.name + " ITTFALSE NAME " + (sym_top().get(i)).name);
       }
       }
       System.err.println( "RETURN FALSE");
