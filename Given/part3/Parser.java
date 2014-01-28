728f5931e61baa24b5f5e892883974b59201400b
Shwoobie <atdwashington@gmail.com>
2014-01-27 17:48:51 -0800

    test print statements double declaration

--- Given/part3/Parser.java
+++ Given/part3/Parser.java
@@ -69,7 +69,9 @@ public class Parser {
     private void assignment() {
         // you'll need to add some code here
         newSym = new Symbol(tok.lineNumber, table.depth, tok.string);
+        System.err.println( "variable " + newSym.name + " linenumber: " + newSym.dec_line + "address:" + &newSym);
         newSym = new Symbol(tok.lineNumber, table.depth, tok.string);
+        System.err.println( "variable " + newSym.name + " linenumber: " + newSym.dec_line + "address:" + &newSym);
         table.checkSym(newSym);
         scan();
         mustbe(TK.ASSIGN);
